edd8f05ad8dd000b33fe65bfb44b7559f8f22e3b
Mehul <65443164+infiniteregrets@users.noreply.github.com>
2022-10-12 12:35:23 -0400

    Update mirrord.rb

--- mirrord.rb
+++ mirrord.rb
@@ -1,10 +1,9 @@
 class Mirrord < Formula
     desc "Connect your local process and your cloud environment"
     homepage "https://mirrord.dev"
-    url "https://github.com/metalbear-co/mirrord/releases/download/2.13.0/mirrord_mac_universal.zip"
-    sha256 "847f3eb90d46293f6f26f1e65eebdfb9712ea9b6e0be8438b72c8d7f20aa083b"
-    license "MIT"
-    head "https://github.com/metalbear-co/mirrord/releases/download/3.0.11-alpha/mirrord_mac_universal.zip"
+    url "https://github.com/metalbear-co/mirrord/releases/download/3.0.20-alpha/mirrord_mac_universal.zip"
+    sha256 "9c61e8949ad75824a92839fd155228b874a0079dd7fb85462521866eaa726c0f"
+    license "MIT"    
   
     def install
       bin.install "mirrord"
